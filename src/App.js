84da558de2e2335299a80d5620ff22631718bffa
itabdur <itabdurrahim22@gmail.com>
2022-06-30 09:53:15 +0600

    tailwinds-css-is-ready

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-5xl text-secondary'>This is abdur rahim</h1>
+     
+<label for="my-modal" class="btn modal-button">open modal</label>
+
+
+<input type="checkbox" id="my-modal" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="modal-action">
+      <label for="my-modal" class="btn">Yay!</label>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default App;
